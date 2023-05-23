dffa899fc5380c65963e674d63c0645efe37ca8e
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-22 20:19:40 -0600

    Add game setup

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
 
+bool gameOver;
+
+void setUp(){
+    
+};
+
+void draw(){
+
+};
+
+void input(){
+
+};
+
+void logic(){
+
+};
 
 int main(){
-    
+
+    setUp();
+    while(!gameOver){
+        draw();
+        input();
+        logic();
+    }
+
     return 0;
-}
+};
